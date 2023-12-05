8a307e7afbc08a40e50c83efca7ec54c5397109b
Soham Ghadge <65226596+sohamghadge24@users.noreply.github.com>
2023-12-06 00:34:48 +0530

    Add files via upload

---
+++ forage-lyft-starter-repo/Tries/OctoprimeTires.py
@@ -0,0 +1,9 @@
+from Tries.Tires import Tires
+
+
+class OctoprimeTires(Tires):
+    def __init__(self, tire_wear):
+        self.tire_wear = tire_wear
+
+    def needs_service(self):
+        return sum(self.tire_wear) >= 3.0
