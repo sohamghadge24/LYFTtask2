c00e79fef69c2bd9033a92c3176f104fcda8e814
Soham Ghadge <65226596+sohamghadge24@users.noreply.github.com>
2023-12-06 00:11:37 +0530

    Add files via upload

---
+++ forage-lyft-starter-repo/CarFactory.py
@@ -0,0 +1,43 @@
+from Battery.NubbinBattery import NubbinBattery
+from Battery.SpindlerBattery import SpindlerBattery
+from car import Car
+from Engine.CapuletEngine import CapuletEngine
+from Engine.SternmanEngine import SternmanEngine
+from Engine.WilloughbyEngine import WilloughbyEngine
+
+
+class CarFactory:
+    @staticmethod
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    @staticmethod
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    @staticmethod
+    def create_palindrome(current_date, last_service_date, warning_light_is_on):
+        engine = SternmanEngine(warning_light_is_on)
+        battery = SpindlerBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    @staticmethod
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    @staticmethod
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(current_date, last_service_date)
+        car = Car(engine, battery)
+        return car
